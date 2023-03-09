d0eaa534c602f188c3d905c3ed1823cf1e3c010f
Octavio Villena <octaviovillena@hotmail.com>
2023-03-08 20:01:49 -0800

    no particles when no message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <ParticlesBackground></ParticlesBackground>
+      {m && n && <ParticlesBackground></ParticlesBackground>}
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
