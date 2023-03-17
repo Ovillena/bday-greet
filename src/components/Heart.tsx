eb2af8b35f6ab153567c8cbab8c8538df3ad1f12
Octavio Villena <octaviovillena@hotmail.com>
2023-03-16 21:52:54 -0700

    added beating heart

---
+++ src/components/Heart.tsx
@@ -0,0 +1,7 @@
+import styles from "@/styles/Heart.module.css";
+
+const Heart = () => {
+  return <div className={styles.heart}></div>;
+};
+
+export default Heart;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import { useRouter } from "next/router";
 import ParticlesBackground from "@/components/ParticlesBackground";
+import Heart from "@/components/Heart";
 
 export default function Home() {
   const router = useRouter();
@@ -44,6 +45,7 @@ export default function Home() {
               ? `${name}${m === "" || n === "" ? "" : ", "} ${message}`
               : ""}
           </p>
+          <Heart />
         </div>
       </main>
     </div>
